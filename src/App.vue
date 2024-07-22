97e6da7b013bea85dd3cc86ddcea613ef80bc3fc
lgzzk <46087700+lgzzk@users.noreply.github.com>
2024-07-22 23:10:21 +0800

    初始化工程

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline text-sky-500">
+    哔哩哔哩 (゜-゜)つロ 干杯~-bilibili
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
