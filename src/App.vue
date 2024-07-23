58b6b3f2ce206c55003322ab15e3e3b7cc734b00
lgzzk <46087700+lgzzk@users.noreply.github.com>
2024-07-23 12:51:21 +0800

    配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 </script>
-
+  <router-view/>
 <template>
-  <h1 class="text-3xl font-bold underline text-sky-500">
-    哔哩哔哩 (゜-゜)つロ 干杯~-bilibili
-  </h1>
+
 </template>
 
 <style scoped>
---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import {createMemoryHistory, createRouter} from 'vue-router'
+
+
+const routes = [
+    {path: '/', component: () => import('../pages/index/index.vue')},
+]
+const router = createRouter({
+    history: createMemoryHistory(),
+    routes,
+})
+export default router
